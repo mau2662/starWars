4ae0ae233be79c60565fbe03dd71351d0b5d5aad
mau2662 <vargas7384@gmail.com>
2021-03-21 01:23:21 +0000

    sidebar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,12 +5,33 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Star wars </span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+				<div className="dropdown">
+					<button
+						className="btn btn-primary dropdown-toggle"
+						type="button"
+						id="dropdownMenuButton"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false">
+						Favorites
+						<span className="badge badge-light">0</span>
+					</button>
+
+					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+						<a className="dropdown-item" href="#">
+							Action
+						</a>
+						<a className="dropdown-item" href="#">
+							Another action
+						</a>
+						<a className="dropdown-item" href="#">
+							Something else here
+						</a>
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,20 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
-
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import Card from "../component/Card";
+export const Home = () => {
+	return (
+		<div className="container">
+			<p className="h2">Characters</p>
+			<div className="scrolling-wrapper-flexbox">
+				<Card> </Card>
+				<Card> </Card>
+				<Card> </Card>
+				<Card> </Card>
+				<Card> </Card>
+				<Card> </Card>
+				<Card> </Card>
+			</div>
+		</div>
+	);
+};
