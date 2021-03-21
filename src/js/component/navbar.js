cc72f8d92dbee708834e5932054cdf40d0c06eee
mau2662 <vargas7384@gmail.com>
2021-03-21 02:03:38 +0000

    icono

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,33 +3,38 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Star wars </span>
-			</Link>
-			<div className="ml-auto">
-				<div className="dropdown">
-					<button
-						className="btn btn-primary dropdown-toggle"
-						type="button"
-						id="dropdownMenuButton"
-						data-toggle="dropdown"
-						aria-haspopup="true"
-						aria-expanded="false">
-						Favorites
-						<span className="badge badge-light">0</span>
-					</button>
+		<nav className="navbar navbar-light bg-light mb-3 ">
+			<div className="container">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1" />
+					<h1 className="float-left">
+						<i className="fab fa-jedi-order" />
+					</h1>
+				</Link>
+				<div className="ml-auto">
+					<div className="dropdown">
+						<button
+							className="btn btn-primary dropdown-toggle"
+							type="button"
+							id="dropdownMenuButton"
+							data-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false">
+							Favorites
+							<span className="badge badge-light">0</span>
+						</button>
 
-					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-						<a className="dropdown-item" href="#">
-							Action
-						</a>
-						<a className="dropdown-item" href="#">
-							Another action
-						</a>
-						<a className="dropdown-item" href="#">
-							Something else here
-						</a>
+						<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a className="dropdown-item" href="#">
+								Action
+							</a>
+							<a className="dropdown-item" href="#">
+								Another action
+							</a>
+							<a className="dropdown-item" href="#">
+								Something else here
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
