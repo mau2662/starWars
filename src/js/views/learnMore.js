946d670ff9dd7e17f8cbf87323bf2e3c6afe07af
mau2662 <vargas7384@gmail.com>
2021-03-28 03:58:19 +0000

    hooks

--- src/js/views/learnMore.js
+++ src/js/views/learnMore.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export const LearnMore = () => {
 	const params = useParams();
+	const [character, setCharacter] = useState({});
+
+	const loadCharacter = async () => {
+		const url = "https://www.swapi.tech/api/people/" + params.uid;
+		const requestOption = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		};
+		let response = await fetch(url, requestOption);
+		let data = await response.json();
+		setCharacter = data;
+	};
+
+	useEffect(() => {
+		loadCharacter();
+	}, []);
+
 	return (
 		<div className="card mb-3">
 			<div className="row no-gutters">
@@ -12,7 +31,7 @@ export const LearnMore = () => {
 				</div>
 				<div className="col-md-8">
 					<div className="card-body">
-						<h5 className="card-title">hi</h5>
+						<h5 className="card-title">{Character.result.properties.name}</h5>
 						<p className="card-text">El id es {params.uid}</p>
 						<p className="card-text">
 							<small className="text-muted">Last updated 3 mins ago</small>
